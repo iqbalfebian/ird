54d4f5061a6aff4c16e5671852d75c0f1454ed55
iqbal <iqbalfebian.krw@gmail.com>
2026-01-16 12:10:44 +0700

    Refactor HomePage.vue for improved code organization and readability

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -26,44 +26,50 @@
           <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button>
       </ion-fab>
-
-
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { actionSheetController, IonCol, IonFab, IonFabButton, IonGrid, IonIcon, IonImg, IonRow } from '@ionic/vue';
-import { camera, trash } from 'ionicons/icons';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { usePhotoGallery } from '@/composables/use-photo-gallery';
-// import { addNewToGallery, photos } from '@/composables/use-photo-gallery';
+import {
+  actionSheetController,
+  IonCol,
+  IonFab,
+  IonFabButton,
+  IonGrid,
+  IonIcon,
+  IonImg,
+  IonRow,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { camera, trash } from "ionicons/icons";
+import { usePhotoGallery, UserPhoto } from "@/composables/use-photo-gallery";
 
-const { photos, addNewToGallery, deletePhoto } = usePhotoGallery()
+const { photos, addNewToGallery, deletePhoto } = usePhotoGallery();
 
 const showActionSheet = async (photo: UserPhoto) => {
   const actionSheet = await actionSheetController.create({
-    header: 'Photos',
+    header: "Photos",
     buttons: [
       {
-        text: 'Delete',
-        role: 'destructive',
+        text: "Delete",
+        role: "destructive",
         icon: trash,
         handler: () => {
           deletePhoto(photo);
         },
       },
       {
-        text: 'Cancel',
+        text: "Cancel",
         icon: close,
-        role: 'cancel',
-        handler: () => {
-          // Nothing to do, action sheet is automatically closed
-        },
+        role: "cancel",
       },
     ],
   });
   await actionSheet.present();
 };
-
 </script>
